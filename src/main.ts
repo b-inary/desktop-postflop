a014f54d8ef45b442354e49d8249574a8fbc4264
Wataru Inariba <oinari17@gmail.com>
2022-06-28 23:11:12 +0900

    feat: disable right click menu

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,6 @@ import App from "./components/App.vue";
 import "./style.css";
 
 createApp(App).use(createPinia()).mount("#app");
+
+// disable the right click menu
+document.addEventListener("contextmenu", (e) => e.preventDefault());
